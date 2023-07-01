4caf703497085bf2874a61f9985b22bd93116a6a
moonjunsain <moonjunsain@gmail.com>
2023-07-01 04:21:42 -0700

    Started working on start function, created timeInterval

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,61 @@
 var scores = 0; 
 var timeLeft = 200;
 var questions = ["What is the capital Uruguay?", "What is the capital of Panama?"];
-var answerChoices = [["Managua", "Santiago", "Monte Video", "Brasilia"], [""]]; // made of arrays of strings
+var answerChoices = [
+    ["Managua", "Santiago", "Monte Video", "Brasilia"], 
+    [""]
+]; // made of arrays of strings
 var correctAnswer = ["Monte Video","Panama City"]; // made of strings
 var currentQuestion = 0;
 
 // All of our selectors
 var questionEl = document.querySelector("#question");
-
+var startBtn = document.querySelector("#start");
+var optionBtn1 = document.querySelector("#option1");
+var optionBtn2 = document.querySelector("#option2");
+var optionBtn3 = document.querySelector("#option3");
+var optionBtn4 = document.querySelector("#option4");
+var optionBtns = [optionBtn1, optionBtn2, optionBtn3, optionBtn4];
+var timeEl = document.querySelector("#time");
+timeEl.textContent = "Time Left: " + timeLeft;
 
 // functions
+function countTime(){
+    var timeInterval = setInterval(function(){
+        if(timeLeft == 0){
+            clearInterval(timeInterval);
+            alert("Time has run out!!");
+            endGame();
+        }else if(timeLeft <= 30){
+            timeLeft--;
+            timeEl.style.color = "red";
+        }else{
+            timeLeft--;
+        }
+        
+        timeEl.textContent = "Time Left: " + timeLeft;
+    }, 1000);
+}
+
 function start() {
     // triggered when they pressed button (event listener)
     // starts the timer
+    countTime();
         // set interval
         // once the timer hits 0, call endGame function
+        
     // hide the start button
+    startBtn.setAttribute("style", "display: none;");
+
     // reveal the options, questions
+    questionEl.textContent = questions[currentQuestion];
+    for(var i = 0; i < optionBtns.length; i++){
+        optionBtns[i].style.display = "block";
+        optionBtns[i].textContent = answerChoices[currentQuestion][i];
+        optionBtns[i].addEventListener("click", nextQuestion);
+    }
+
+
 }
 
 function nextQuestion(event){
@@ -52,3 +91,4 @@ function saveInitials(){
 }
 
 // event listener to triger start function
+startBtn.addEventListener("click", start);
