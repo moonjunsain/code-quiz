806f0934e3a14db78ddd28ec82220b8bb0030bb1
moonjunsain <moonjunsain@gmail.com>
2023-07-04 21:52:38 -0700

    Made sure that all the score and initials get displayed properly on high score page

--- assets/js/highScoreScript.js
+++ assets/js/highScoreScript.js
@@ -4,11 +4,14 @@ var scores = JSON.parse(localStorage.getItem('score'));
 
 // selectors
 var ranking = document.querySelector("#ranks");
-var lists = document.createElement("li");
+
 
 for(var i = 0; i < initials.length; i++){
-    
+    let lists = document.createElement("li");
+    lists.textContent = initials[i] + ": " + scores[i];
+    ranking.appendChild(lists);
 }
-lists.textContent = initials + ": " + scores;
-ranking.appendChild(lists);
+var allLists = ranking.querySelectorAll("li");
+
+
 
