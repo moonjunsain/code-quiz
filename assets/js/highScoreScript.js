7ee2efec56ad0a1f95bb8b7cf07c7654e28a119d
moonjunsain <moonjunsain@gmail.com>
2023-07-05 08:22:16 -0700

    made an array to store user initial and score data

--- assets/js/highScoreScript.js
+++ assets/js/highScoreScript.js
@@ -5,12 +5,23 @@ var scores = JSON.parse(localStorage.getItem('score'));
 // selectors
 var ranking = document.querySelector("#ranks");
 
+var combinedData = [];
 
+// combine data into object array
 for(var i = 0; i < initials.length; i++){
+    combinedData.push({userInitial: initials[i], userScore: scores[i]});
+}
+
+// sorting it from the highest score to lowest
+combinedData.sort();
+
+// Putting the list items all to ranking
+for(var i = 0; i < combinedData.length; i++){
     let lists = document.createElement("li");
-    lists.textContent = initials[i] + ": " + scores[i];
+    lists.textContent = combinedData[i].userInitial + ": " + combinedData[i].userScore;
     ranking.appendChild(lists);
 }
+
 var allLists = ranking.querySelectorAll("li");
 
 
